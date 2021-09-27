41bfdef1ee36ab9216b5c62254365d7f23f4dd98
sourav391 <souravpatil31@gmail.com>
2021-09-27 15:41:40 +0530

    docs :change

--- index.js
+++ index.js
@@ -1,53 +1,52 @@
-var readlinesync = require ("readline-sync");
+var readlinesync = require("readline-sync");
 
-var score=0;
+var score = 0;
 
 var playername = readlinesync.question("what is yoyur name? ");
-console.log ("welcome " , playername , " to do you know sourav!");
-console.log ("-----------------------------------------------------------")
+console.log("welcome ", playername, " to do you know sourav!");
+console.log("-----------------------------------------------------------")
 
-function quiz( question, answer) {
+function quiz(question, answer) {
   var userans = readlinesync.question(question);
 
   if (userans === answer) {
     console.log("you are right");
     score = score + 1;
+  } else {
+    console.log("you are wrong")
   }
-    else { 
-      console.log("you are wrong")
-    }
 
-    console.log("you current score is" , score);
+  console.log("you current score is", score);
 
 }
 //without array.
 // quiz("which city i live in ?", "panvel");
 
 
-var questions = [ { 
-  question: "where do i live ? ",
-  answer: "panvel"
-},
-  { 
-  question: "which is my school ? ",
-  answer:  "NHPS"
-
-   
-},
+var questions = [{
+    question: "where do i live ? ",
+    answer: "panvel"
+  },
   {
-  question: "my hobby ? ",
-  answer: "playing",
-},
+    question: "which is my school ? ",
+    answer: "NHPS"
 
-{
-  question: "my hometown ? ",
-  answer: "panvel",
-},
 
-{
-  question: "my pet name ? ",
-  answer: "bruno",
-},
+  },
+  {
+    question: "my hobby ? ",
+    answer: "playing",
+  },
+
+  {
+    question: "my hometown ? ",
+    answer: "panvel",
+  },
+
+  {
+    question: "my pet name ? ",
+    answer: "bruno",
+  },
 
 ];
 
@@ -62,6 +61,6 @@ var questions = [ {
 // }
 
 
-for (let qestion in questions){
-  quiz(questions[qestion].question , questions[qestion].answer);
+for (let qestion in questions) {
+  quiz(questions[qestion].question, questions[qestion].answer);
 }
